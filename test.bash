72086857950e993db0108437d15530ed7d1f2685
Akiyama Kaito <s22c1002vv@s.chibakoudai.jpgit config  --global user.email s22c1002vv@s.chibakoudai.jpgit config  --global user.email s22c1002vv@s.chibakoudai.jp>
2024-01-09 18:02:48 +0900

    First Commit

--- test.bash
+++ test.bash
@@ -10,24 +10,24 @@ ng () {
 res=0
 
 # テストケース1
-out=$(echo -e "10\n3" | python3 your_python_program.py)
-[ "${out}" = "10個のきびだんごを3人で分けると、一人あたり3個で、1個余ります" ] || ng ${LINENO}
+out=$(echo -e "10\n3" | python3 Division.py)
+[ "${out}" = "10個のきびだんごを3人で分けると、一人あたり3.0個で、1.0個余ります" ] || ng ${LINENO}
 
 # テストケース2
-out=$(echo -e "あ\n3" | python3 your_python_program.py)
+out=$(echo -e "あ\n3" | python3 Division.py)
 [ "$?" = 1 ] || ng ${LINENO}
 
 # テストケース3
-out=$(echo -e "\n3" | python3 your_python_program.py)
+out=$(echo -e "\n3" | python3 Division.py)
 [ "$?" = 1 ] || ng ${LINENO}
 [ "${out}" = "" ] || ng ${LINENO}
 
 # テストケース4
-out=$(echo -e "15\n0" | python3 your_python_program.py)
+out=$(echo -e "15\n0" | python3 Division.py)
 [ "$?" = 1 ] || ng ${LINENO}
 
 # テストケース5
-out=$(echo -e "15\n3" | python3 your_python_program.py)
+out=$(echo -e "15\n3" | python3 Division.py)
 [ "${out}" = "15個のきびだんごを3人で分けると、一人あたり5.0個で、0.0個余ります" ] || ng ${LINENO}
 
 [ "$res" = 0 ] && echo "OK"
