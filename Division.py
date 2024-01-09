437ee0bf6231e5ca6644f69de59adc94bcd80ecb
Akiyama Kaito <s22c1002vv@s.chibakoudai.jpgit config  --global user.email s22c1002vv@s.chibakoudai.jpgit config  --global user.email s22c1002vv@s.chibakoudai.jp>
2024-01-09 17:17:31 +0900

    First Commit

--- Division.py
+++ Division.py
@@ -2,10 +2,10 @@
 #SPDX-FileCopyrightText: 2023 Kaito Akiyama
 #SPDX-License-Identifier: BSD-3-Clause
 
-x = int(input("飴の個数を入力："))
-y = int(input("人数を入力: "))
+x = float(input("きびだんごの個数を入力："))
+y = float(input("人数を入力: "))
 
 mul = x // y
 amari = x % y
 
-print(f"{x}個の飴を{y}人で分けると、一人あたり{mul}個で、{amari}個余ります")
+print(f"{x}個のきびだんごを{y}人で分けると、一人あたり{mul}個で、{amari}個余ります")
