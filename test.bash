c4981317fc77c68f8aaf525428b670d07e355779
Akiyama Kaito <s22c1002vv@s.chibakoudai.jpgit config  --global user.email s22c1002vv@s.chibakoudai.jpgit config  --global user.email s22c1002vv@s.chibakoudai.jp>
2024-01-10 16:10:57 +0900

    First Commit

--- test.bash
+++ test.bash
@@ -3,20 +3,19 @@
 # SPDX-License-Identifier: BSD-3-Claunse
 
 ng () {
-	echo NG at Line $1
-	res=1
+    echo "NG at Line $1"
+    res=1
 }
 
 res=0
 
-out=$(echo 8 | python3 array.py)
+python3 array.py > output.txt
+
+out=$(cat output.txt)
+
 [ "${out}" = "和: 15.999999985098839" ] || ng ${LINENO}
 
-out=$(echo あ | python3 array.py)
-[ "$?" = 1 ] || ng ${LINENO}
-
-out=$(echo | python3 array.py)
-[ "$?" = 1 ] || ng ${LINENO}
+rm output.txt
 
 [ "$res" = 0 ] && echo "OK"
 exit $res
