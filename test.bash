920b813c4e307ce213c00338507ac895838a6ec1
Akiyama Kaito <s22c1002vv@s.chibakoudai.jpgit config  --global user.email s22c1002vv@s.chibakoudai.jpgit config  --global user.email s22c1002vv@s.chibakoudai.jp>
2024-01-12 23:18:19 +0900

    First Commit

--- test.bash
+++ test.bash
@@ -12,5 +12,9 @@ res=0
 out=$(echo 8 | python3 array | grep "^和:")
 [ "${out}" = "和: 15.999999985098839" ] || ng ${LINENO}
 
+out=$(echo "invalid" | python3 array 2>&1)
+[ "$?" = 1 ] || ng ${LINENO}
+[ "${out}" = "Error: Invalid input provided." ] || ng ${LINENO}
+
 [ "$res" = 0 ] && echo "OK"
 exit $res
